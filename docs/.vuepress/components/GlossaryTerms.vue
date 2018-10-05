c10059d0e67dee39ed09ac75bdf40e5187cc6bc6
Jonathan Dupré <hello@jonathandupre.com>
2018-10-05 12:06:45 -0400

    split glossary terms by first letter

--- docs/.vuepress/components/GlossaryTerms.vue
+++ docs/.vuepress/components/GlossaryTerms.vue
@@ -1,12 +1,24 @@
 <template>
-  <ul>
-    <li v-for="page in entries">
-      <router-link :to="getRoute(page.path)">
-        {{ page.title }}
-        </router-link>
-    </li>
-  </ul>
+  <article>
+    <section v-for="group in groups">
+      <h2>{{ group.name }}</h2>
+
+      <ul>
+        <li v-for="entry in group.entries">
+          <router-link :to="entry.path">
+            {{ entry.title }}
+          </router-link>
+        </li>
+      </ul>
+    </section>
+  </article>
 </template>
+
+<style scoped>
+ul {
+  list-style: none;
+}
+</style>
 
 <script>
 export default {
@@ -14,21 +26,33 @@ export default {
     pages: Array
   },
   computed: {
-    entries () {
-      return this.pages.filter(this.isGlossaryPage)
+    groups () {
+      var pages = this.pages.filter(this.isGlossaryPage)
+      var letters = {}
+
+      pages.forEach(page => {
+        var letter = page.title[0]
+        if (!letters[letter]) letters[letter] = { pages: [] }
+        letters[letter].pages.push(page)
+      })
+
+      return Object.keys(letters).map(letter => ({
+        name: letter,
+        entries: letters[letter].pages.map(page => ({
+          title: page.title,
+          path: page.path
+        }))
+      }))
     }
   },
   methods: {
-    getRoute (path) {
-      return path
-    },
     isGlossaryPage: function (page) {
       if (page.title === 'Glossary') return false
 
       var components = page.path.split('/')
 
       return components.length > 2 && components[1] === 'glossary'
-    }  
+    }
   }
 }
 
