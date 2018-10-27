adcd465285fb98fb351d22a9e34d41d87d588398
adasq <asnaebaem@gmail.com>
2018-10-27 15:38:33 +0200

    typo fixes / forcing script to fail for errors

--- scripts/sitemap-generator.js
+++ scripts/sitemap-generator.js
@@ -4,12 +4,12 @@ const { createSitemap } = require('sitemap')
 const requires = require('esm')(module)
 
 const STEEM_DOCS_URL = 'https://steemdocs.net'
-const BUEPRESS_PUBLIC_DIR = join(__dirname, '../docs/.vuepress/public')
+const VUEPRESS_PUBLIC_DIR = join(__dirname, '../docs/.vuepress/public')
 
-// laoding site pages definition
+// loading site pages definition
 const { siteData: { pages } } = requires(resolve('node_modules/@vuepress/core/.temp/internal/siteData'))
 
-//generation metadata for xml generator
+//generate metadata for "sitemap" generator
 const urls = pages.map(toSitemapUrlEntry)
 
 const sitemap = createSitemap({
@@ -18,14 +18,16 @@ const sitemap = createSitemap({
   urls,
 })
 
-if (!existsSync(BUEPRESS_PUBLIC_DIR)) {
-  console.error('.vuepress public dir does not exist:', BUEPRESS_PUBLIC_DIR)
+if (!existsSync(VUEPRESS_PUBLIC_DIR)) {
+  console.error('.vuepress public dir does not exist:', VUEPRESS_PUBLIC_DIR)
   process.exit(1)
 }
 
-writeFile(resolve(BUEPRESS_PUBLIC_DIR, 'sitemap.xml'), sitemap.toString(), (err) => {
-  if (!err) console.log(`sitemap generated in: "${BUEPRESS_PUBLIC_DIR}"`)
-  else console.error(err)
+writeFile(resolve(VUEPRESS_PUBLIC_DIR, 'sitemap.xml'), sitemap.toString(), (err) => {
+  if (!err) return console.log(`sitemap generated in: "${VUEPRESS_PUBLIC_DIR}"`)
+  
+  console.error(err)
+  process.exit(1)
 })
 
 // map vuepress page definition to "sitemap" generator
