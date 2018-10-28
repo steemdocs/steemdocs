e275921eaa7f11803bcfa8f115522d8325154337
adasq <asnaebaem@gmail.com>
2018-10-28 09:34:00 +0100

    simple UT added

---
+++ tests/sitemap.js
@@ -0,0 +1,30 @@
+const tape = require('tape')
+const convert = require('xml-js')
+const fs = require('fs')
+const path = require('path')
+
+tape('saves URL to home page', function (assert) {
+    const pages = getPagesBySitemap(getSitemap())
+
+    const homePage = pages.find(page => page.url === 'https://steemdocs.net/')
+
+    assert.ok(homePage, 'home page is defined')
+    assert.end()
+})
+
+function getPagesBySitemap(sitemap) {
+    const pages = sitemap.elements[0].elements.map(entry => {
+        const [url, lastmod] = entry.elements
+        return {
+            url: url.elements[0].text,
+            lastmod: lastmod.elements[0].text
+        }
+    })
+
+    return pages
+}
+
+function getSitemap() {
+    const sitemapXmlContent = fs.readFileSync(path.join(__dirname, '../', '/docs/.vuepress/dist/sitemap.xml'))
+    return JSON.parse(convert.xml2json(sitemapXmlContent))
+}
